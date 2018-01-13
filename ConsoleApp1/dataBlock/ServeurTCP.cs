869714a25b1064c6279e8d7981416bd9426c629e
DESKTOP-GIB7BNL\Djypzi <antoineblanc269@laposte.net>
2018-01-13 18:56:44 -0500

    TCP for client/serveur

---
+++ ConsoleApp1/dataBlock/ServeurTCP.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace myBlockChain.dataBlock
+{
+    class ServeurTCP
+    {
+
+        /*   Server Program    */
+        private int portNumber;
+        private Boolean stop = true;
+
+        public ServeurTCP(int portNumber)
+        {
+            this.portNumber = portNumber;
+        }
+
+        public void TCP()
+        {
+            try
+            {
+                IPAddress ipAd = IPAddress.Parse("172.21.5.99");
+                // use local m/c IP address, and 
+                // use the same in the client
+
+                /* Initializes the Listener */
+                TcpListener myList = new TcpListener(ipAd, 8001);
+
+                /* Start Listeneting at the specified port */
+                myList.Start();
+
+                Console.WriteLine("The server is running at port 8001...");
+                Console.WriteLine("The local End point is  :" +
+                                  myList.LocalEndpoint);
+
+                while(true && stop){
+                    Console.WriteLine("Waiting for a connection.....");
+
+                    Socket clientSoc = myList.AcceptSocket();
+                    Console.WriteLine("Connection accepted from " + clientSoc.RemoteEndPoint);
+                }
+                
+
+                
+                myList.Stop();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+        }
+
+    }
+}
+
---
+++ ConsoleApp1/dataBlock/TCPServerThread.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace myBlockChain.dataBlock
+{
+    class TCPServerThread
+    {
+        Socket clientSoc;
+
+        public TCPServerThread(Socket clientSoc)
+        {
+            this.clientSoc = clientSoc;
+        }
+
+        public void receiveData()
+        {
+            //byte[] b = new byte[100];
+            //int k = this.clientSoc.Receive(b);
+            Console.WriteLine("Recieved...");
+            int bytesRead = 0;
+            StringBuilder messageBuilder = new StringBuilder();
+            using (NetworkStream ns = new NetworkStream(clientSoc))
+            {
+                int messageChunkSize = 10;
+                do
+                {
+                    byte[] chunks = new byte[messageChunkSize];
+                    bytesRead = ns.Read(chunks, 0, chunks.Length);
+                    messageBuilder.Append(Encoding.UTF8.GetString(chunks));
+                }
+                while (bytesRead != 0);
+            }
+            //for (int i = 0; i < k; i++)
+            //  Console.Write(Convert.ToChar(b[i]));
+
+            Console.WriteLine(messageBuilder);
+
+            //ASCIIEncoding asen = new ASCIIEncoding();
+            //this.clientSoc.Send(asen.GetBytes("The string was recieved by the server."));
+            //Console.WriteLine("\nSent Acknowledgement");
+            /* clean up */
+            this.clientSoc.Close();
+        }
+    }
+}
